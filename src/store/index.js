0c457d2e2fd9a48594f4e1fe60e2990822c4e298
CalebCouch <Calebblackcat@Gmail.com>
2022-03-06 09:52:57 -0500

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ export default createStore({
     //     });
     // },
     GetAllCups (state) {
-      fetch("http://162.0.224.190:3000/")
+      fetch("http://localhost/api")
         .then(res => res.json())
         .then(data => {
           state.quotes = data;
